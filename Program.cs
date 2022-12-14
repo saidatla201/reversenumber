3a37c23c255406857ad2026605f99ff98e077ebf
saidatla201 <saidatla201@gmail.com>
2022-12-14 09:52:11 +0530

    [saidatla] adding reversenumber program

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversenumber
+{
+    internal class Program
+    {
+         static void Main(string[]args)
+        {
+            Program.reverse( 123);
+        }
+        public static void reverse(int num )
+
+        {
+            Console.WriteLine("enter the input number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int remainder = 0, reverse = 0;
+            while (num != 0)
+            {
+                remainder = num % 10;
+                reverse = (reverse * 10) + remainder;
+                num = num / 10;
+
+            }
+            Console.WriteLine("the reverse num is :" + reverse);
+            Console.ReadLine();
+        
+
+        }
+    }
+}
